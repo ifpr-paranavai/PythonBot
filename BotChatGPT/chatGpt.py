45b47639a817ac2d86cfcba4febded73e2ee4196
Gabriel Moya Nascimento <36999588+GabrielMoyaNascimento@users.noreply.github.com>
2023-08-25 20:51:57 -0300

    Criando bot com a api do chatGPT

---
+++ BotChatGPT/chatGpt.py
@@ -0,0 +1,28 @@
+import openai
+
+openai.api_key = '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'
+
+def get_completion(prompt, model="gpt-4"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    return response.choices[0].message["content"]
+
+def main():
+    print("Bem-vindo ao ChatBot! Digite 'sair' para encerrar.")
+    
+    while True:
+        pergunta = input("Você: ")
+        
+        if pergunta.lower() == "sair":
+            print("Até logo!")
+            break
+        
+        resposta = get_completion(pergunta)
+        print("Bot:", resposta)
+
+if __name__ == "__main__":
+    main()
